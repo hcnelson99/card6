991d912961ea540c1fad17d9f39b3ccc336340a9
Henry Nelson <hcnelson99@gmail.com>
2023-07-24 16:19:20 -0400

    .

--- src/routes/editor/+page.svelte
+++ src/routes/editor/+page.svelte
@@ -1,5 +1,49 @@
 <script>
-  import CardEditor from '$lib/CardEditor.svelte';
+  import Card from '$lib/Card.svelte';
+  import Canvas from './Canvas.svelte';
+
+  let title;
+  let cardType;
+  let rules;
+  let mana;
+  let power;
+  let image;
 </script>
 
-<CardEditor />
+<div class="wrapper">
+  <form>
+    <label>
+      Title:
+      <input bind:value={title} />
+    </label>
+    <label>
+      Card Type:
+      <input bind:value={cardType} />
+    </label>
+    <label>
+      Rules:
+      <textarea bind:value={rules} />
+    </label>
+    <label>
+      Mana:
+      <input bind:value={mana} />
+    </label>
+    <label>
+      Power:
+      <input bind:value={power} />
+    </label>
+  </form>
+  <Canvas bind:image />
+  <Card {title} {cardType} {rules} {mana} {power} {image} />
+</div>
+
+<style>
+  .wrapper {
+    display: flex;
+    gap: 10px;
+  }
+
+  label {
+    display: block;
+  }
+</style>
